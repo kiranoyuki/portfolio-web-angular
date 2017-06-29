4f49aae2d0866e7a34a3915654fbd44495e37c94
Anh Le <aql25@drexel.edu>
2017-06-29 17:33:50 -0400

    decent style on home - working on img hover of projects

--- angular-2-starter/app/shared/projects.ts
+++ angular-2-starter/app/shared/projects.ts
@@ -7,15 +7,17 @@ export const ourProjects: Project[] = [
     {
         id: 101,
         name: 'Project1',
+        category: 'company website',
         info: 'Pede accumsan, lorem pellentesque lectus scelerisque consequat sit, ligula tristique tincidunt et, ' +
         'eget at in elit in enim adipiscing',
-        image: 'app/shared/images/1.jpg'
+        image: 'app/shared/images/sample1.jpg'
     },
     {
         id: 102,
         name: 'Project1',
+        category: 'company website',
         info: 'Pede accumsan, lorem pellentesque lectus scelerisque consequat sit, ligula tristique tincidunt et, ' +
         'eget at in elit in enim adipiscing',
-        image: 'app/shared/images/2.jpg'
+        image: 'app/shared/images/sample1.jpg'
     }
 ];
